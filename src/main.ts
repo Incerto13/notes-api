28241cbe8da261918272545a422f4a6f84bc2893
Hezekiah <hezekiah@Hezekiahs-MacBook-Pro.local>
2024-05-26 03:05:48 -0400

    resolve cors

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   //   credentials: true,
   // });
 
-  var whitelist = ['http://localhost:3000', 'http://54.242.227.79:3000', 'http://solace.incertotech.com/api'];
+  var whitelist = ['http://localhost:3000', 'http://54.242.227.79:3000', 'http://solace.incertotech.com','*'];
   app.enableCors({
     origin: function (origin, callback) {
       if (whitelist.indexOf(origin) !== -1) {
