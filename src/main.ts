5c5b91117e8cb90d65de9be1bd3ac4e74a7b2c97
Hezekiah <hezekiah@Hezekiahs-MacBook-Pro.local>
2024-05-26 00:56:24 -0400

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,30 @@ async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000/api',
+  //     'http://54.242.227.79:3000/api',
+  //     'http://solace.incertotech.com/api',
+  //     '*',
+  //   ],
+  //   methods: ["GET", "POST", "DELETE", "PATCH"],
+  //   credentials: true,
+  // });
+
+  var whitelist = ['http://localhost:3000', 'http://54.242.227.79:3000', 'http://solace.incertotech.com','*'];
   app.enableCors({
-    origin: [
-      'http://localhost:3000/api',
-      'http://54.242.227.79:3000/api',
-      'http://solace.incertotech.com/api',
-      '*',
-    ],
-    methods: ["GET", "POST", "DELETE", "PATCH"],
-    credentials: true,
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log("allowed cors for:", origin)
+        callback(null, true)
+      } else {
+        console.log("blocked cors for:", origin)
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: "GET,PATCH,POST,DELETE,UPDATE,OPTIONS",
   });
   app.useGlobalPipes(new ValidationPipe());
   const port = 3000;
