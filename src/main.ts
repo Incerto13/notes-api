48d52a86fff87b8dd60e4ad3067a9cb5937b1777
Hezekiah <hezekiah@Hezekiahs-MacBook-Pro.local>
2024-05-26 09:26:34 -0400

    allow for when

--- src/main.ts
+++ src/main.ts
@@ -7,21 +7,10 @@ async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  // app.enableCors({
-  //   origin: [
-  //     'http://localhost:3000/api',
-  //     'http://54.242.227.79:3000/api',
-  //     'http://solace.incertotech.com/api',
-  //     '*',
-  //   ],
-  //   methods: ["GET", "POST", "DELETE", "PATCH"],
-  //   credentials: true,
-  // });
-
   var whitelist = ['http://localhost:8080', 'http://54.242.227.79:8080', 'http://solace.incertotech.com:8080'];
   app.enableCors({
     origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
         console.log("allowed cors for:", origin)
         callback(null, true)
       } else {
